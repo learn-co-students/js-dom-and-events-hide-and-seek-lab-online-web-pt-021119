b667d0bcf0cf31f3dc3048e28dc086bbe034cccf
Tracy Ngo <tngo1206@gmail.com>
2019-09-14 17:56:51 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  return  document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+
+function deepestChild(){
+  return  document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  const rankedList = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < rankedList.length; i++){
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n
+  }
+}
